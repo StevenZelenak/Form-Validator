ec3152a72e8ba80f75d9fc61f798650530fe135d
StevenZelenak <stevenmzelenak@gmail.com>
2020-02-08 12:00:19 -0800

    updated js and css

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Getting elements by Id and turning them into variables
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
@@ -13,13 +14,14 @@ const showError = (input, message) => {
     small.innerText = message;
 }
 
+//show input succes border
 const showSucces = (input, message) => {
 
     const formcontrol = input.parentElement;
     formcontrol.className = 'form-control success';
 }
 
-
+//Checks if username is empty or full
 const checkUsername = (e) => {
     e.preventDefault();
     if (username.value === '') {
@@ -29,10 +31,12 @@ const checkUsername = (e) => {
     }
 };
 
+//checks if email is valid format
 const emailIsValid = (email) => {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test((email).toLowerCase());
 }
 
+//checks if there is any input in email
 const checkEmail = (e) => {
     e.preventDefault();
     if (email.value === '') {
@@ -44,24 +48,31 @@ const checkEmail = (e) => {
     }
 };
 
+//Checks if password is empty or not and if they are the correct length
 const checkPassword = (e) => {
     e.preventDefault();
     if (password.value === '') {
         showError(password, 'Password is required');
+    } else if (password.value.length < 6) {
+        showError(password, 'Password should be 6 characters or more');
     } else {
         showSucces(password)
     }
 };
 
+//checks if password2 is empty or not and if they match
 const checkPassword2 = (e) => {
     e.preventDefault();
     if (password2.value === '') {
         showError(password2, 'Password is required');
+    } else if (password2.value !== password.value) {
+        showError(password2, `Passwords don't match`);
     } else {
         showSucces(password2)
     }
 };
 
+//Where all the EvenListeners are stored
 const events = () => {
     form.addEventListener('submit', checkUsername);
     form.addEventListener('submit', checkEmail);
@@ -69,8 +80,10 @@ const events = () => {
     form.addEventListener('submit', checkPassword2);
 };
 
+//function to initialize funtcions
 const init = () => {
     events();
 };
 
+//initializes js code
 init();
